e53911d98271a684dc44370a8214ef19a03c3770
Kristjan Kivikangur <Kristjan.Kivikangur@ttrk.ee>
2016-05-02 10:55:59 +0300

    Loon projekti katsetamiseks

---
+++ TestProjekt/TestProjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjekt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Kristjan oli siin");
+		}
+	}
+}
